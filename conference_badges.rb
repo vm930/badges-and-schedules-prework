16fe0c29cd5283ec039bb1c94f6b111e9bc99b5b
abstract-animation-9914 <vm930@hotmail.com>
2019-01-16 23:16:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,28 @@
 
-def badge_maker(name)
-  puts "Hello, my name is #{name}."
-end 
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator (arr_names)
-  arr_badege_messages = []
-  arr_badege_messages = badge_maker(arr_names)
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
-def assign_rooms(arr_names)
-  
-end 
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
 
-def printer(arr_names)
-  
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
