bf2964b00d84a6280b37ecf0c92fc9bdc68eacb6
abstract-animation-9914 <vm930@hotmail.com>
2019-01-16 23:16:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator (arr_names)
+  arr_badege_messages = []
+  arr_badege_messages = badge_maker(arr_names)
+end
+
+def assign_rooms(arr_names)
+  
+end 
+
+def printer(arr_names)
+  
+end
